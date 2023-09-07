15bcc8f8e9f0ae2bca3b72ee04f1b2b7a7384362
issacmukara <144338116+issacmukara@users.noreply.github.com>
2023-09-07 17:10:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 // routes
 app.use("/api/v1/auth", authRoutes);
